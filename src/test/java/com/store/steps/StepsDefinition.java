e9041d880b166ff50b9158d1fdbec1ba6c691fa8
lfc <lufer.lfcc@gmail.com>
2020-01-12 17:01:04 -0800

    scond Tc was added And Steps definition with no function

--- src/test/java/com/store/steps/StepsDefinition.java
+++ src/test/java/com/store/steps/StepsDefinition.java
@@ -3,6 +3,7 @@ package com.store.steps;
 import com.store.pages.BottomMenu;
 import com.store.pages.Singin;
 import com.store.pages.StorePage;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -13,7 +14,7 @@ public class StepsDefinition {
     private BottomMenu bottomMenu;
     private StorePage store;
 
-    public StepsDefinition(Singin singin, BottomMenu bottomMenu, StorePage store){
+    public StepsDefinition(Singin singin, BottomMenu bottomMenu, StorePage store) {
         this.singin = singin;
         this.bottomMenu = bottomMenu;
         this.store = store;
@@ -33,5 +34,39 @@ public class StepsDefinition {
     public void iExpectTheHasAsDiscount(String itemName, String percentageExpect) {
         String percentageReduction = store.getPercentageReduction(itemName);
         assertEquals(percentageReduction, percentageExpect);
+    }
+
+
+    @And("I click on {string} button")
+    public void iClickOnButton(String buttonName) {
+        store.clickOn(buttonName);
+    }
+
+    @And("I hover {string} then add to the card")
+    public void iHoverThenAddToTheCard(String itemName) {
+        store.addToCardHover(itemName);
+    }
+
+    @And("I go to the Cart dropdown and select {string}")
+    public void iGoToTheCartDropdownAndSelectCheckout() {
+
+    }
+
+    @Then("I expect {string} is displayed in summary table and the availability is {string}")
+    public void iExpectPrintedChiffonDressIsDisplayedInSummaryTableAndTheAvailabilityIsInStock() {
+
+    }
+
+    @And("I remove {string} from the summary table")
+    public void iRemoveBlouseFromTheSummaryTable() {
+    }
+
+    @And("I expect {string} is no longer displayed in the summary table")
+    public void iExpectIsNoLongerDisplayedInTheSummaryTable(String arg0) {
+        
+    }
+
+    @And("I expect the total amount to pay is {string}")
+    public void iExpectTheTotalAmountToPayIs(String arg0) {
     }
 }
--- src/test/resources/dress_verification.feature
+++ src/test/resources/dress_verification.feature
@@ -1,6 +1,20 @@
 Feature: Dress verification
 
-  Scenario: Verify discount
+#  Scenario: Verify discount
+#    Given Open Automation Practice page
+#    When I click on "Best sellers" option in bottom menu
+#    Then I expect the "Printed Chiffon Dress" has "-20%" as discount
+
+  Scenario: verify discount
     Given Open Automation Practice page
     When I click on "Best sellers" option in bottom menu
-    Then I expect the "Printed Chiffon Dress" has "-20%" as discount
+    And I hover "Printed Chiffon Dress" then add to the card
+    And I click on "Continue shopping" button
+    And I hover "Blouse" then add to the card
+    And I click on "Continue shopping" button
+    And I go to the Cart dropdown and select "Checkout"
+    Then I expect "Printed Chiffon Dress" is displayed in summary table and the availability is "In stock"
+    And I expect "Blouse" is displayed in summary table and the availability is "In stock"
+    And I remove "Blouse" from the summary table
+    And I expect "Blouse" is no longer displayed in the summary table
+    And I expect the total amount to pay is "$18.40"
