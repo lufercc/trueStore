674c4fc781f538908109162b0ca322a9cbccd644
lfc <lufer.lfcc@gmail.com>
2020-01-12 17:10:23 -0800

    added hover function in WebDriverActions add click By location funtion

--- src/main/java/com/store/core/WebDriverActions.java
+++ src/main/java/com/store/core/WebDriverActions.java
@@ -3,6 +3,7 @@ package com.store.core;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -10,10 +11,12 @@ public class WebDriverActions {
 
     private WebDriver driver;
     private WebDriverWait wait;
+    private Actions actions;
 
     public WebDriverActions(WebDriver driver, WebDriverWait wait){
         this.driver = driver;
         this.wait = wait;
+        this.actions = new Actions(driver);
     }
 
     public void click(WebElement element){
@@ -21,8 +24,19 @@ public class WebDriverActions {
         element.click();
     }
 
+    public void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+        driver.findElement(locator).click();
+    }
+
     public String getText(By selector) {
         wait.until(ExpectedConditions.visibilityOfElementLocated(selector));
         return driver.findElement(selector).getText();
+    }
+
+    public void hover(By selector) {
+        WebElement target = driver.findElement(selector);
+        wait.until(ExpectedConditions.visibilityOf(target));
+        actions.moveToElement(target).build().perform();
     }
 }
--- src/main/java/com/store/pages/StorePage.java
+++ src/main/java/com/store/pages/StorePage.java
@@ -1,12 +1,36 @@
 package com.store.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.HashMap;
 
 public class StorePage extends AbstractPage{
 
+    @FindBy(css = "[title='Continue shopping']")
+    private WebElement continueShoppingButton;
+
+    private HashMap<String, WebElement> buttons = new HashMap<>();
+
     public static final String PERCENTAGE_REDUCTION = "//a[@title='%s']/ancestor::h5/following-sibling::div[@class='content_price']/child::span[@class='price-percent-reduction']";
+    public static final String STORE_ITEM = "//a[@title='%s']/ancestor::div[@class='product-image-container']";
+    public static final String ADD_TO_CARD = "//a[@title='%s']/ancestor::h5/following-sibling::div[@class='button-container']/child::a[@title='Add to cart']";
+
+    public StorePage() {
+         buttons.put("Continue shopping", continueShoppingButton);
+    }
 
     public String getPercentageReduction(String dressName){
         return action.getText(By.xpath(String.format(PERCENTAGE_REDUCTION, dressName)));
+    }
+
+    public void addToCardHover(String itemName) {
+        action.hover(By.xpath(String.format(STORE_ITEM, itemName)));
+        action.click(By.xpath(String.format(ADD_TO_CARD, itemName)));
+    }
+
+    public void clickOn(String buttonName) {
+        action.click(buttons.get(buttonName));
     }
 }
